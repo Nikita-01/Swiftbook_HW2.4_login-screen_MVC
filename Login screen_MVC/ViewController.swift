235bc3e7223d37213acc128dbb5f24174b59bee3
Nikita Ryzhkin <timongo@bk.ru>
2021-10-29 22:19:00 +0400

    Initial Commit

---
+++ Login screen_MVC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Login screen_MVC
+//
+//  Created by Никита Рыжкин on 29.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
